a2e2727e2a7ee55be98d5f915e953869cf13763c
hopefullyme <hope1fuller@gmail.com>
2025-08-04 19:11:01 -0500

    start day3 part 2

--- solutions/day3.py
+++ solutions/day3.py
@@ -1,29 +1,30 @@
 from utils.reading import file_to_list_of_lines
 def solve():
-    input = file_to_list_of_lines('inputs/3.txt')
-    # input = file_to_list_of_lines('inputs/test3.txt')
+    # input = file_to_list_of_lines('inputs/3.txt')
+    input = file_to_list_of_lines('inputs/test3.txt')
 
     bitsInPos = [[bitstr[i] for bitstr in input] for i in range(len(input[0]))]
     # print(bitsInPos)
     gamma = getGamma(bitsInPos)
     print(gamma)
-    epsilon = getEpsilon(bitsInPos)
-    print(epsilon)
+    # epsilon = getEpsilon(bitsInPos)
 
-    print("\n")
-    print(gamma * epsilon)
+    # oxygen generator rating
+    ogr = getOxyRate(input, gamma)
 
 def getGamma(bitLists):
     # most common bit from each position
     gamma = ''
     for bitlist in bitLists:
         counts = countBits(bitlist)
+        print(counts)
         if counts[0] > counts[1]:
             gamma += '0'
+        elif counts[0] == counts[1]:
+            gamma += '-'
         else:
             gamma += '1'
-    gDec = int(gamma, 2)
-    return gDec
+    return gamma
     
 def getEpsilon(bitLists):
     # least common bit from each position
@@ -32,10 +33,30 @@ def getEpsilon(bitLists):
         counts = countBits(bitlist)
         if counts[0] < counts[1]:
             ep += '0'
+        elif counts[0] == counts[1]:
+            ep += '-'
         else:
             ep += '1'
-    epDec = int(ep, 2)
-    return epDec
+    return ep
+
+def getOxyRate(nums, gamma):
+    numList = nums.copy()
+    # gamma has the most common bit for each bit positoin
+    for pos, bit in enumerate(gamma):
+        if bit == '-':
+            bit = '1'
+        nextList = []
+        for num in numList:
+            if hasBitInPos(num, bit, pos):
+                nextList.append(num)
+        numList = nextList
+        print(numList)
+    
+        
+    
+
+def hasBitInPos(str, bit, pos):
+    return str[pos] == bit
 
 def countBits(bitList):
     zeroes = bitList.count('0')
