4df82258c7c63ecc645d5910bd4c40d7793f192c
hopefullyme <hope1fuller@gmail.com>
2025-08-14 18:41:04 -0500

    Finished day5p1

--- solutions/day5.py
+++ solutions/day5.py
@@ -2,11 +2,12 @@ from utils.reading import file_to_list_of_lines
 import re
 
 def solve():
-    puz_input = file_to_list_of_lines('inputs/test5.txt')
-    # puz_input = file_to_list_of_lines('inputs/5.txt')
+    # puz_input = file_to_list_of_lines('inputs/test5.txt')
+    puz_input = file_to_list_of_lines('inputs/5.txt')
     coords = parse_input(puz_input)
     seafloor_map = map_coords(coords)
-    
+    overlaps = count_overlaps(seafloor_map)
+    print(overlaps)
 
 def parse_input(input):
     myregx = re.compile(r'(\d+),(\d+) -> (\d+),(\d+)')
@@ -17,10 +18,11 @@ def parse_input(input):
 def map_coords(coords):
     (xsize, ysize) = map_size(coords)
     da_map = [[0 for _ in range(ysize+1)] for _ in range(xsize+1)]
-    print(da_map)
-    for (x1, y1, x2, y2) in coords:
-        if x1 == x2 or y1 == y2:
-            pass
+    for ptlist in coords:
+        line = ends_to_pts(ptlist)
+        for pt in line:
+            da_map[pt[0]][pt[1]] += 1
+    return da_map 
 
 def map_size(coords):
     maxX = 0
@@ -35,3 +37,26 @@ def map_size(coords):
         if y2 > maxY:
             maxY = y2
     return (maxX, maxY)
+
+def ends_to_pts(endpoints):
+    (x1, y1, x2, y2) = endpoints
+    if x1 == x2:
+        yMin = min(y1, y2)
+        yMax = max(y1, y2)
+        points = [(x1, y) for y in range(yMin, yMax+1)]
+        return points
+    if y1 == y2:
+        xMin  = min(x1, x2)
+        xMax = max(x1, x2)
+        points = [(x, y1) for x in range(xMin, xMax+1)]
+        return points
+    return []    
+
+def count_overlaps(sf_map):
+    overlaps = 0
+    ol_min = 2 
+    for row in sf_map:
+        for i in row:
+            if i >= ol_min:
+                overlaps += 1
+    return overlaps
