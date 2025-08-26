d09a6f8747093956f934b4553f7437a32047008f
hopefullyme <hope1fuller@gmail.com>
2025-08-26 10:36:59 -0500

    new fuel cost calculation

--- solutions/day7.py
+++ solutions/day7.py
@@ -3,10 +3,15 @@ from statistics import median
 
 def solve():
     # inputs = read_csv('inputs/test7.txt')
-    inputs = read_csv('inputs/7.txt')
-    t_pos = median(inputs)
-    fuel_cost = 0
-    for crab in inputs:
-        fuel_cost += abs(crab - t_pos)
-    print(fuel_cost)
+    # inputs = read_csv('inputs/7.txt')
+    # t_pos = median(inputs)
+    # fuel_cost = 0
+    # for crab in inputs:
+    #     fuel_cost += abs(crab - t_pos)
+    answer = crab_move_cost(16, 5)
+    print(answer)
+    assert(answer == 66)
     
+
+def crab_move_cost(init, target):
+    return sum(range(abs(target - init)+1))
