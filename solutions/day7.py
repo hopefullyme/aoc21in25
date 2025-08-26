e82b74af94f0830e778c7106032cfc3b310b6087
hopefullyme <hope1fuller@gmail.com>
2025-08-26 10:33:07 -0500

    day7part1 solution was v simple

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,4 +1,12 @@
 from utils.reading import read_csv
+from statistics import median
 
 def solve():
-    pass
+    # inputs = read_csv('inputs/test7.txt')
+    inputs = read_csv('inputs/7.txt')
+    t_pos = median(inputs)
+    fuel_cost = 0
+    for crab in inputs:
+        fuel_cost += abs(crab - t_pos)
+    print(fuel_cost)
+    
