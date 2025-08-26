a910af2fdf563fcf3e9a4c398878874268d5d900
hopefullyme <hope1fuller@gmail.com>
2025-08-26 10:47:01 -0500

    brute force took 13s but... it worked day7.2 complete

--- solutions/day7.py
+++ solutions/day7.py
@@ -2,15 +2,22 @@ from utils.reading import read_csv
 from statistics import median
 
 def solve():
-    inputs = read_csv('inputs/test7.txt')
-    # inputs = read_csv('inputs/7.txt')
+    # inputs = read_csv('inputs/test7.txt')
+    inputs = read_csv('inputs/7.txt')
     crab_min = min(inputs)
     crab_max = max(inputs)
+    costs = []
     for target in range(crab_min, crab_max):
         fuel_cost = 0
         for crab in inputs:
             fuel_cost += crab_move_cost(crab, target)
-        print(f"Move to {target} costs {fuel_cost}")
+        
+        # Move to {target} costs {fuel_cost}
+        costs.append((target, fuel_cost))
+    
+
+    least_cost = min(costs, key=lambda x: x[1])
+    print(least_cost)
     
 
 def crab_move_cost(init, target):
