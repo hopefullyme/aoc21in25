641c9ab03c9136f1c7970acf176bed5acdec583f
hopefullyme <hope1fuller@gmail.com>
2025-08-26 10:41:55 -0500

    Test naive bruteforce solution & it seems to work

--- solutions/day7.py
+++ solutions/day7.py
@@ -2,15 +2,15 @@ from utils.reading import read_csv
 from statistics import median
 
 def solve():
-    # inputs = read_csv('inputs/test7.txt')
+    inputs = read_csv('inputs/test7.txt')
     # inputs = read_csv('inputs/7.txt')
-    # t_pos = median(inputs)
-    # fuel_cost = 0
-    # for crab in inputs:
-    #     fuel_cost += abs(crab - t_pos)
-    answer = crab_move_cost(16, 5)
-    print(answer)
-    assert(answer == 66)
+    crab_min = min(inputs)
+    crab_max = max(inputs)
+    for target in range(crab_min, crab_max):
+        fuel_cost = 0
+        for crab in inputs:
+            fuel_cost += crab_move_cost(crab, target)
+        print(f"Move to {target} costs {fuel_cost}")
     
 
 def crab_move_cost(init, target):
