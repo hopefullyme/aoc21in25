c6fdb7d075ce4b79980e8c7ed8b687369c64aeb1
hopefullyme <hope1fuller@gmail.com>
2025-09-08 11:58:10 -0500

    used breadthfirst search to explore basins

--- solutions/day9.py
+++ solutions/day9.py
@@ -1,12 +1,16 @@
 from utils.reading import file_to_list_of_lines
+from collections import deque
 
 def solve():
     inputs = file_to_list_of_lines('inputs/9.txt')
     grid_str = [list(line) for line in inputs]
     grid = [[int(h) for h in row] for row in grid_str]
     points = get_low_points(grid)
-    risk_levels = [h+1 for h in points]
-    print(sum(risk_levels))
+    basins = [get_basin(point, grid) for point in points]
+    b = sorted(basins)
+    
+    answer = b[-1] * b[-2] * b[-3]
+    print(answer)
 
 def get_low_points(grid):
     points = []
@@ -24,5 +28,31 @@ def get_low_points(grid):
                 neighbors.append(grid[y][x+1])
             
             if height < min(neighbors):
-                points.append(height)
+                points.append((y, x))
     return points
+
+def get_basin(start, grid):
+    basin = bfs(grid, start)
+    return len(basin)
+
+def bfs(grid, point):
+    height = grid[point[0]][point[1]]
+    q = deque([point])
+    visited = [point]
+
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]    
+
+    while q:
+        y,x = q.popleft()
+        height = grid[y][x]
+        for dy, dx in directions:
+            ny, nx = y + dy, x + dx
+            
+            # check we're not revisiting points
+            if (ny, nx) in visited:
+                continue
+            # check edges and height
+            if 0 <= ny < len(grid) and 0 <= nx < len(grid[ny]) and grid[ny][nx] >= height and grid[ny][nx] != 9:
+                q.append((ny, nx))
+                visited.append((ny, nx))
+    return visited
