fc02fd0396239e8e79bcd66adb4faa78feba86ed
hopefullyme <hope1fuller@gmail.com>
2025-09-16 12:01:49 -0500

    created a "completion string" to close all the braces left open, and convert that into a score as described in problem statement

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,21 +1,29 @@
 from utils.reading import file_to_list_of_lines
 from collections import deque
+from statistics import median
 
-SCORE_TABLE = {
+SCORE_TABLE_SYNTAX = {
     ')' :3,
     ']': 57 ,
     '}': 1197 ,
     '>': 25137 
 }
 
+SCORE_TABLE_COMPLETION = {
+    ')': 1 ,
+    ']': 2 ,
+    '}': 3 ,
+    '>': 4 
+}
+
 def solve():
     input = file_to_list_of_lines("inputs/10.txt")
-    score = 0
+    scores = []
     for line in input:
-        bad = process_line(line)
-        if bad:
-            score += SCORE_TABLE[bad]
-    print(f"Score: {score}")
+        result = process_line(line)
+        if result:
+            scores.append( ac_score(result))
+    print(median(scores))
 
 def process_line(str):
     stack = deque()
@@ -28,8 +36,27 @@ def process_line(str):
                 (ch == ']' and top != '[') or \
                 (ch == '}' and top != '{') or \
                 (ch == '>' and top != '<') :
-                return ch
-    # if stack:
-    #     print("incomplete")
-    # else:
-    #     print("seems ok")
+                return ''
+    
+    completion = ""
+    while stack:
+        top = stack.pop()
+        if top == '(':
+            completion += ')' 
+        elif top == '[':
+            completion += ']'
+        elif top == '{':
+            completion += '}'
+        elif top == '<':
+            completion += '>'
+    
+    return completion
+    
+
+def ac_score(cstr):
+    '''AutoComplete Score takes a completion string and scores it according to rules'''
+    score = 0
+    for char in cstr:
+        score *= 5
+        score += SCORE_TABLE_COMPLETION[char]
+    return score
